4144a4da0c808034e9b63bd50fb6f3706c5eec3a
emmalite <emmanuelemafolomi@gmail.com>
2022-07-22 15:42:09 -0700

    commited message

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+
+
+
+
+def safe_function(fct, *args):
+
+    try:
+
+        result = fct(*args)
+
+        return result
+
+    except Exception as error:
+
+        print("{}{}".format("Exception: ", error), file=sys.stderr)
+
+        return None
